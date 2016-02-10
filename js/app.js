b9914a1ea71420fc583e7eead4b35edfc79c39d3
Davide Rocco <daviderocco@MacBook-di-Davide.local>
2016-02-10 15:48:44 +0100

    sprint1 solutions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+var array_scores =[];
+       var scoreIT = 0;
+       var scoreSP = 0;
+       var scoreEN = 0; 
+var indexMax = 0;
+
 var ParlaModel = {
   
    /* Langs contain the list of language with their corresponding
@@ -14,6 +20,37 @@ var ParlaModel = {
    getLanguageScore : function (phrase, language){
      // write your code here, but don't add any additional 
      // parameters to the the function
+       
+       if( language == ita ){
+           for( var i in language ){
+            if( phrase.indexOf( language[i] ) != -1 ){
+               scoreIT++;
+           }else{
+               continue;
+           }
+        }
+       }
+       
+       if( language == eng ){
+           for( var y in language ){
+            if( phrase.indexOf( language[y] ) != -1 ){
+               scoreEN++;
+           }else{
+               continue;
+           }
+        }
+       }
+       
+       if( language == spa ){
+           for( var z in language ){
+            if( phrase.indexOf( language[z] ) != -1 ){
+               scoreSP++;
+           }else{
+               continue;
+           }
+        }
+       }
+
    },
   
    /* Returns the name of the language in which the phrase
@@ -22,7 +59,44 @@ var ParlaModel = {
     */
    detectLanguage : function (phrase) {
      // write your code here, but don't add any additional 
-     // parameters to the the function     
-   }
+     // parameters to the the function 
+      
+       return languages[indexMax].name;
+   },
+    
+    arrayscoreMaxPos: function(){
+        array_scores.push(scoreIT,scoreEN,scoreSP);
+        var maxx= Math.max.apply(Math,array_scores); 
+        for(var l in array_scores){
+            if( array_scores[l] == maxx ){
+               indexMax = array_scores[l];
+            }else{
+                continue;
+            }
+        }
+    }
          
 };
+
+var View = {
+	layout: function(lang_pos){
+		$(".lang-name").text(lang_pos);
+	}
+};
+
+var Controller = {
+	init: function(){
+		$("button").click(function(){
+            ParlaModel.getLanguageScore($("input").val(), ita);
+            ParlaModel.getLanguageScore($("input").val(), eng);
+            ParlaModel.getLanguageScore($("input").val(), spa);
+            ParlaModel.arrayscoreMaxPos();
+            View.layout( ParlaModel.detectLanguage($("input").val()) );
+        });
+	}
+};
+
+$(document).ready(function(){
+    	Controller.init();
+});
+
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -3,8 +3,48 @@
 
 describe("Parla", function() {
 
-  it("should...", function() {  
+  it("should give the correct score of language", function() {  
+        expect( ParlaModel.getLanguageScore("non lo so", ita) ).toBe(1);
+  });   
     
-  });    
+    it("should give the correct score of language", function() {  
+        expect( ParlaModel.getLanguageScore("non lo so", eng) ).toBe(0);
+  }); 
+    
+    it("should give the correct score of language", function() {  
+        expect( ParlaModel.getLanguageScore("non lo so", spa) ).toBe(0);
+  });
+    
+    it("should give the correct score of language", function() {  
+        expect( ParlaModel.getLanguageScore("the best of you", eng) ).toBe(2);
+  });
+    
+    it("should give the correct score of language", function() {  
+        expect( ParlaModel.getLanguageScore("fdf fdgdggg", eng) ).toBe(0);
+  });
+    
+    it("should give the correct score of language", function() {  
+        expect( ParlaModel.getLanguageScore("nullfdfq", ita) ).toBe(0);
+  });
+    
+    it("should give the correct score of language", function() {  
+        expect( ParlaModel.getLanguageScore("nullfdfq", spa) ).toBe(0);
+  });
+    
+    it("should give the correct language", function() {  
+        expect( ParlaModel.detectLanguage("non lo so") ).toEqual(languages[0].name);
+  });
+    
+    it("should give the correct language", function() {  
+        expect( ParlaModel.detectLanguage("que el") ).toEqual(languages[1].name);
+  });
+    
+    it("should give the correct language", function() {  
+        expect( ParlaModel.detectLanguage("que el") ).toEqual(languages[1].name);
+  });
+    
+    it("should give the correct language", function() {  
+        expect( ParlaModel.detectLanguage("the you") ).toEqual(languages[2].name);
+  });
  
 });
